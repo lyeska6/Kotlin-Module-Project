3a26646880c79d2fc27837099d8bcb48b9f7af79
lyeska6 <liz-on-chik@mail.ru>
2024-05-29 21:40:08 +0300

    приложение для заметочек

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Добро пожаловать в приложение \"Заметки\"!")
+    val archiveList: MutableList<NoteArchive> = mutableListOf()
+    val archiveMenu: Menu<NoteArchive> = Menu("архив", "архивов", archiveList)
+    archiveMenu.input({NoteArchive.createArchive()}, { it ->
+        val noteMenu: Menu<Note> = Menu("заметку", "заметок архива ${archiveList[it].name}", archiveList[it].noteList)
+        noteMenu.input({archiveList[it].createNote()}, {that ->
+            println("Содержимое данной заметки:")
+            println(archiveList[it].noteList[that].text)
+        })
+    })
+    println("Спасибо, что воспользовались приложением \"Заметки\". До новых встреч!")
 }
